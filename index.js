6caafa05de29eac0209a329d3deb5c41c659089c
Chance Mims <chance@Chances-MacBook-Air.local>
2020-01-28 12:17:25 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    let leftNumber = dodger.style.left.replace('px', '');
+    let number = parseInt(leftNumber, 10);
+
+    if (number > 0){
+        dodger.style.left = `${number - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    let leftNumber = dodger.style.left.replace('px', '');
+    let number = parseInt(leftNumber, 10);
+
+
+
+    if (number > 0){
+        dodger.style.left = `${number + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if (e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+    else if (e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+});
